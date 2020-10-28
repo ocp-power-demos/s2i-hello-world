56a40e7555d7b11850b281d9cd7b0f47916cc6fb
Deepak C Shetty <dpkshetty@gmail.com>
2020-10-28 16:28:41 +0530

    Add /version

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World again - by Deepak'
+
+@app.route('/version')
+def get_version():
+    return 'App version: <b>1.0</b>'
     
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
