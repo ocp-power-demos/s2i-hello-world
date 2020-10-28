96f98fc340d65d811924591e6e145a44e3d936d6
Deepak C Shetty <dpkshetty@gmail.com>
2020-10-28 16:31:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def hello_world():
 
 @app.route('/version')
 def get_version():
-    return 'App version: <b>1.0</b>'
+    return 'App version: <b>2.0</b>'
     
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
