c06070787b5dee65b438e6bcb77e280c65badffe
Deepak C Shetty <dpkshetty@gmail.com>
2020-11-05 20:32:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World Again - by Deepak'
+    return 'Hello, World Again - by Deepak, Using route URL as webhook'
 
 @app.route('/version')
 def get_version():
