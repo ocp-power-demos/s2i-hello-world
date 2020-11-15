4f11a9bf32a39a35f5dab9250a24a0cc61fb8a18
Deepak C Shetty <dpkshetty@gmail.com>
2020-11-15 15:33:46 +0530

    Add /test endpoint
    Added /test endpoint

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ def hello_world():
 @app.route('/version')
 def get_version():
     return 'App version (using generic webhook): <b>2.0</b>'
-    
+
+@app.route('/test')
+def get_version():
+    return 'You are accessing /test endpoint'
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
